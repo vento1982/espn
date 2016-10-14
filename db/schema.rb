4ca0ab1d46fb9bf5b566d8d75716c6262ccb98fa
vento <marcin.vento@gmail.com>
2016-10-14 22:37:32 +0200

    Add Index, New and Create actions with View

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013191051) do
+ActiveRecord::Schema.define(version: 20161013210525) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
-    t.text     "decription"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
